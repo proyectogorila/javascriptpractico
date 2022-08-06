595c2557377076e4157e2b76b419ba2fec2d6062
Boris Barrios <proyectogorila80@gmail.com>
2022-08-05 22:44:19 -0400

    se concluyo clase 13

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const parrafito = document.querySelector(".parrafito");
-const pid = document.querySelector("#pid");
-const input = document.querySelector("input");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const boton = document.querySelector("#btnCalcular")
+const resultado = document.querySelector(".result");
 
-console.log(input.value);
-
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-});
-
-
-h1.innerHTML = "Patito <br> feo";
-
-p.innerHTML = "sdfsfdsfds";
-
-console.log(h1.getAttribute('pantalla'));
-/* console.log(h1.getAttribute('class'));
-h1.setAttribute('class', 'rojo'); */
-h1.classList.add('rojo');
-h1.classList.remove('verde');
-input.value = '456';
-
-const img = document.createElement('img');
-img.setAttribute('src', 'https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg')
-
-pid.innerHTML = '';
-pid.append(img);
+function oneclick() {
+    resultado.innerText = 'resultado es = ' + (parseInt(input1.value) + parseInt(input2.value));
+}
