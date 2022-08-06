d8c5fb15a683e365cd36f70656836bb5ec44c41f
Boris Barrios <proyectogorila80@gmail.com>
2022-08-05 22:59:24 -0400

    se concluyo clase 13

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
 const boton = document.querySelector("#btnCalcular")
 const resultado = document.querySelector(".result");
+const formulario = document.querySelector("#formulario");
 
-function oneclick() {
+formulario.addEventListener('click', btnOnClick);
+
+
+function btnOnClick() {
+
     resultado.innerText = 'resultado es = ' + (parseInt(input1.value) + parseInt(input2.value));
 }
